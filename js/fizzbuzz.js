ecbbc6caba14e2ffe884aefcbc954c8864e68d2d
MCoppinDev <mattcoppinger@protonmail.com>
2022-11-03 16:27:46 -0400

    added code and fixed link targets

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,35 +1,40 @@
-document.getElementById("fizzButton").addEventListener("click", function () {
-  let num1 = document.getElementById("input1").value;
-  let num2 = document.getElementById("input2").value;
-  let limit = document.getElementById("limit").value;
-
-  fizzBuzz(num1, num2,limit)
-
-});
+var fizzBuzzResults = document.getElementById("fizzBuzzResults");
+var resultDOM = document.getElementById("resultCard");
+var resultMessage = document.getElementById("resultMessage");
+var hideButton = document.getElementById("hideButton");
 
 function fizzBuzz(num1, num2,limit) {
 
-  let results = "";
+  let results = [];
 
   for (let i = 1; i <= limit; i++) {
 
     if (i % num1 == 0 && i % num2 == 0) {
-      results += 'FizzBuzz,  ';
+      results.push(' ✨FizzBuzz⚡ ');
     }
 
     else if (i % num1 == 0) {
-      results += 'Fizz,  ';
+      results.push(' Fizz!✨ ');
     }
       
     else if (i % num2 == 0) {
-      results += 'Buzz,  ';
+      results.push(' Buzz!⚡ ');
     }
       
-    else results += i + ',  ';
-
+    else results.push(` ${i} `);
   };
 
-  document.get
-  
-  document.getElementById("results").innerHTML= `${results}`
+  return results;
 };
+
+
+document.getElementById("fizzButton").addEventListener("click", function () {
+  let num1 = document.getElementById("input1").value;
+  let num2 = document.getElementById("input2").value;
+  let limit = document.getElementById("limit").value;
+  resultDOM.style.visibility = "visible"
+
+  
+  resultMessage.innerHTML = fizzBuzz(num1, num2, limit);
+
+});
